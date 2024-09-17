f496149c9889edd115e2471d0122967739602cc3
haitan2408 <haitan28102408@gmail.com>
2024-09-17 21:30:45 +0700

    haitt up demo tổng quan js

---
+++ ss6/like.js
@@ -0,0 +1,25 @@
+// external
+//   function: hàm
+// addEventListener: lắng nghe sự kiện click.
+// Khi sự kiện click xảy ra thì nó sẽ thực hiện function và hiển thị alert
+let btn = document.getElementById("btn");
+btn.addEventListener("click", function () {
+    btn.style.backgroundColor="blue";
+    btn.style.color="white";
+    btn.innerText="Unlike";
+})
+
+// document.getElementById("text").addEventListener("click", function () {
+//     alert("hi")
+// })
+
+let a = +prompt("a")
+console.log(a)
+let b = +prompt("b")
+console.log(b)
+alert(a + b)
+
+
+
+
+
